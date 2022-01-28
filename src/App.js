9e411f4b1a8d5f0be1e1d5c56a62eb7822edf51f
htmlHxcker <clintonadeleke@gmail.com>
2022-01-28 09:12:49 +0000

    [FEAT]:App now uses ReactRouter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import RouterConfig from './navigation/RouterConfig';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <RouterConfig />
+    </BrowserRouter>
   );
 }
 
