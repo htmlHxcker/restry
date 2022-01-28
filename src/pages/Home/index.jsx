607d68a5823a26a1a7f1f84e299aed170fa28eb0
htmlHxcker <clintonadeleke@gmail.com>
2022-01-28 11:19:15 +0000

    [FEAT]:Add NavBar to Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBar from '../../components/NavBar';
 
 function Home() {
   return (
     <>
+      <NavBar />
       <h2>Hello, User</h2>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
     </>
