eea66ed67925c7daf56c756b12c61d474af8d071
htmlHxcker <clintonadeleke@gmail.com>
2022-01-28 09:20:50 +0000

    [FEAT]:Create Home Page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <h2>Hello, User</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </>
+  );
+}
+
+export default Home;
