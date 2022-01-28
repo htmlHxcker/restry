931879262dae7f5151cc9118735241c0fc598caa
htmlHxcker <clintonadeleke@gmail.com>
2022-01-28 11:06:15 +0000

    [FEAT]:Create basic NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div className="flex">
+      <h1>Restry</h1>
+      <div>
+        <p>Stats</p>
+        <p>Account</p>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
