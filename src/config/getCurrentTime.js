37f293c5c816e894600d416fb8a1ff4f0cd973d1
htmlHxcker <clintonadeleke@gmail.com>
2022-01-28 13:58:00 +0000

    [FEAT]:Create a util for getting time

---
+++ src/config/getCurrentTime.js
@@ -0,0 +1,25 @@
+function getCurrentTime() {
+  const date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let session = 'AM';
+
+  if (hours === 0) {
+    hours = 12;
+  }
+  if (hours > 12) {
+    hours = 12;
+    session = 'PM';
+  }
+
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  const time = `${hours}:${minutes}:${seconds} ${session}`;
+
+  return time;
+}
+
+export default getCurrentTime;
