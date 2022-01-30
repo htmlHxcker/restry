6cb5facdeefbb9111d451a31276d3ffc175ce5c1
htmlHxcker <clintonadeleke@gmail.com>
2022-01-30 21:43:44 +0000

    [FEAT]:Import MusicPlayer component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FiEdit } from 'react-icons/fi';
 import NavBar from '../../components/NavBar';
 import getCurrentTime from '../../config/getCurrentTime';
+import MusicPlayer from './components/MusicPlayer';
 import StartPomodoro from './components/StartPomodoro';
 
 function Home() {
@@ -18,11 +19,11 @@ function Home() {
         <h3>{currentTime.greeting}</h3>
       </div>
       <StartPomodoro />
-      <div>
+      <div className="flex justify-between footer">
         <button type="button" title="Create new to-do item">
           <FiEdit size={30} />
         </button>
-
+        <MusicPlayer />
       </div>
 
     </>
