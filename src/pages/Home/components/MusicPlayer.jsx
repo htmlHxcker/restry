b2da4912a4e5c298dcb71db44a02ff8ef9152a98
htmlHxcker <clintonadeleke@gmail.com>
2022-02-01 13:10:54 +0000

    [FEAT]:Ability to pause song

--- src/pages/Home/components/MusicPlayer.jsx
+++ src/pages/Home/components/MusicPlayer.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { FiPause, FiPlay } from 'react-icons/fi';
 
 function MusicPlayer({ tracks }) {
   const [trackIndex, setTrackIndex] = useState(0);
@@ -14,13 +15,39 @@ function MusicPlayer({ tracks }) {
   const intervalRef = useRef();
   const isReady = useRef(false);
 
-  const {duration} = audioRef.current;
+  const { duration } = audioRef.current;
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  audioRef.current.addEventListener('ended', () => {
+    if (trackIndex < tracks.length - 1) {
+      setTrackIndex(trackIndex + 1);
+    } else {
+      setTrackIndex(0);
+    }
+  });
 
   return (
-    <>
-      <p>Music Player</p>
-      {tracks}
-    </>
+    <div className="flex">
+      <img src={image} alt={`Cover art for ${title} by ${artist}`} className="cover-art" />
+      <div>
+        <h2>{title}</h2>
+        <h3>{artist}</h3>
+      </div>
+      <div className="flex">
+        {isPlaying ? (
+          <button type="button" aria-label="Play" onClick={() => { setIsPlaying(true); }}><FiPlay /></button>
+        ) : (
+          <button type="button" aria-label="Pause" onClick={() => { setIsPlaying(false); }}><FiPause /></button>
+        )}
+      </div>
+    </div>
   );
 }
 MusicPlayer.propTypes = {
