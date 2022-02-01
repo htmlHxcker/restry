681be5bad7bb2d3f9295ca70ccd52ecd5e78295b
htmlHxcker <clintonadeleke@gmail.com>
2022-02-01 10:37:26 +0000

    [FEAT]: Setup state and refs

--- src/pages/Home/components/MusicPlayer.jsx
+++ src/pages/Home/components/MusicPlayer.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 function MusicPlayer({ tracks }) {
+  const [trackIndex, setTrackIndex] = useState(0);
+  const [trackProgress, setTrackProgress] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const {
+    title, artist, image, audioSrc,
+  } = tracks[trackIndex];
+
+  const audioRef = useRef(new Audio(audioSrc));
+  const intervalRef = useRef();
+  const isReady = useRef(false);
+
+  const {duration} = audioRef.current;
+
   return (
     <>
       <p>Music Player</p>
