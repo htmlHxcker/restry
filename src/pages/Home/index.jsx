9e878ae59a2b8924f79682aa81ce2e41a275c3c9
htmlHxcker <clintonadeleke@gmail.com>
2022-02-02 09:53:49 +0000

    [FIX]:Remove time feature
    The time was re-rendering every second, creating multiple audio objects

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FiEdit } from 'react-icons/fi';
 import NavBar from '../../components/NavBar';
-import getCurrentTime from '../../config/getCurrentTime';
+import tracks from '../../tracks';
 import MusicPlayer from './components/MusicPlayer';
 import StartPomodoro from './components/StartPomodoro';
 
 function Home() {
-  const [currentTime, setCurrentTime] = useState(getCurrentTime());
-  setInterval(() => {
-    setCurrentTime(getCurrentTime);
-  }, 1000);
-
   return (
     <>
       <NavBar />
-      <div>
-        <h2>{currentTime.time}</h2>
-        <h3>{currentTime.greeting}</h3>
-      </div>
       <StartPomodoro />
       <div className="flex justify-between footer">
         <button type="button" title="Create new to-do item">
           <FiEdit size={30} />
         </button>
-        <MusicPlayer />
+        <MusicPlayer tracks={tracks} />
+
       </div>
 
     </>
