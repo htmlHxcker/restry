c4da268c0491d40ff4a5dc400c8ebc6aaa43e6f2
htmlHxcker <clintonadeleke@gmail.com>
2022-02-02 09:52:03 +0000

    [FEAT}:Create Demo array of songs.

---
+++ src/tracks.js
@@ -0,0 +1,13 @@
+import winter from './assets/audio/Winter.mp3';
+import notTheOnlyOne from './assets/audio/Im_Not_the_Only_One.mp3';
+
+const tracks = [
+  {
+    title: 'Winter', artist: 'Alexander Nakarada', audioSrc: winter,
+  },
+  {
+    title: 'Not the only one', artist: 'Sam Smith', audioSrc: notTheOnlyOne,
+  },
+];
+
+export default tracks;
