4211659a4b0958d685ade3d454573d7e6e8e4c37
htmlHxcker <clintonadeleke@gmail.com>
2022-02-12 11:15:11 +0000

    FEAT: Wrap home in grid

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { FiEdit } from 'react-icons/fi';
-import NavBar from '../../components/NavBar';
 import tracks from '../../tracks';
 import MusicPlayer from './components/MusicPlayer';
 import StartPomodoro from './components/StartPomodoro';
 
 function Home() {
   return (
-    <>
-      <NavBar />
-      <StartPomodoro />
-      <div className="flex justify-between footer">
-        <button type="button" title="Create new to-do item">
-          <FiEdit size={30} />
-        </button>
-        <MusicPlayer tracks={tracks} />
+    <div className="grid">
+      <div className="logo"><h1>Restry</h1></div>
+      <div className="main"><StartPomodoro /></div>
 
+      <button type="button" title="Create new to-do item" className="todo">
+        <FiEdit size={30} />
+      </button>
+      <div className="player">
+        <MusicPlayer tracks={tracks} />
       </div>
 
-    </>
+    </div>
   );
 }
 
