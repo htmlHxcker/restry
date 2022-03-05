a4c519d46e95ba4be12a735c47c717c73f9f5ada
htmlHxcker <clintonadeleke@gmail.com>
2022-03-05 12:46:50 +0000

    [FEAT]:Add Navbar to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,9 @@
 import React from 'react';
-import { FiEdit } from 'react-icons/fi';
-import tracks from '../../tracks';
-import MusicPlayer from './components/MusicPlayer';
-import StartPomodoro from './components/StartPomodoro';
+import NavBar from '../../components/NavBar';
 
 function Home() {
   return (
-    <div className="grid">
-      <div className="logo"><h1>Restry</h1></div>
-      <div className="main"><StartPomodoro /></div>
-
-      <button type="button" title="Create new to-do item" className="todo">
-        <FiEdit size={30} />
-      </button>
-      <div className="player">
-        <MusicPlayer tracks={tracks} />
-      </div>
-
-    </div>
+    <NavBar />
   );
 }
 
