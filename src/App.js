fb6861f2cb58127b37b30d8be8035b5d2adb4574
htmlHxcker <clintonadeleke@gmail.com>
2022-03-07 13:51:00 +0000

    [FEAT]:Add NavBar to all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import RouterConfig from './navigation/RouterConfig';
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <RouterConfig />
     </BrowserRouter>
   );
