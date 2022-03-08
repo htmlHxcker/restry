c93c3b6ea67465572fb2a17518a63aa6dadb4e8a
htmlHxcker <clintonadeleke@gmail.com>
2022-03-08 13:28:45 +0000

    [FEAT]:Create Tasks Component

--- src/pages/Home/components/Tasks/index.jsx
+++ src/pages/Home/components/Tasks/index.jsx
@@ -3,18 +3,20 @@ import './index.scss';
 
 function Tasks() {
   return (
-    <>
-      <h2 className="text-white heading-3 screen-heading">Tasks</h2>
+    <div className="tasks">
+      <h2 className="text-white heading-3 page-title">Tasks</h2>
       <div>
         <ul>
-          <li className="task-card text-white">Task 1</li>
+          <li className="task-card text-white">Complete Bojack Horseman</li>
           <li className="task-card text-white">Cook for Tosin</li>
-          <li className="task-card text-white">Task 3</li>
+          <li className="task-card text-white">Buy Food for Temi</li>
+          <li className="task-card text-white">Task 4</li>
+          <li className="task-card text-white">Task 4</li>
           <li className="task-card text-white">Task 4</li>
         </ul>
       </div>
       <button type="button" className="btn new-task-btn">New Task</button>
-    </>
+    </div>
   );
 }
 
