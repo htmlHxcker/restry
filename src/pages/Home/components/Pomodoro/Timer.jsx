4bb82621e4728f4dd210be3a1ad1643c3bc1d3f3
htmlHxcker <clintonadeleke@gmail.com>
2022-03-10 10:43:16 +0000

    [FEAT]:Create Progress meter and text

--- src/pages/Home/components/Pomodoro/Timer.jsx
+++ src/pages/Home/components/Pomodoro/Timer.jsx
@@ -3,13 +3,42 @@ import PropTypes from 'prop-types';
 import './timer.scss';
 
 function Timer({
-  size, strokeWidth, percentage, color,
+  size, strokeWidth, percentage,
 }) {
   const viewBox = `0 0 ${size} ${size}`;
   const radius = (size - strokeWidth) / 2;
+  const circumference = 2 * (Math.PI * radius);
+  const dash = (percentage * circumference) / 100;
+
   return (
     <svg width={size} height={size} viewBox={viewBox}>
-      <circle fill="none" stroke="#ccc" cx={size / 2} cy={size / 2} r={radius} strokeWidth={`${strokeWidth}px`} />
+      <circle
+        className="circular-path"
+        cx={size / 2}
+        cy={size / 2}
+        r={radius}
+        strokeWidth={`${strokeWidth}px`}
+      />
+      <circle
+        className="progress"
+        fill="none"
+        cx={size / 2}
+        cy={size / 2}
+        r={radius}
+        strokeWidth={`${strokeWidth}px`}
+        transform={`rotate(-90 ${size / 2} ${size / 2})`}
+        strokeDasharray={[dash, circumference - dash]}
+        strokeLinecap="round"
+      />
+      <text
+        className="timer-text"
+        x="50%"
+        y="50%"
+        dy="20px"
+        textAnchor="middle"
+      >
+        {`${percentage}%`}
+      </text>
     </svg>
   );
 }
@@ -18,6 +47,5 @@ Timer.propTypes = {
   size: PropTypes.number.isRequired,
   strokeWidth: PropTypes.number.isRequired,
   percentage: PropTypes.number.isRequired,
-  color: PropTypes.string.isRequired,
 };
 export default Timer;
