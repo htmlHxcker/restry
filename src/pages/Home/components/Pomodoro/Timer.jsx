92cb3d2f843839535e5f8c5a69caf7b31d6c5f38
htmlHxcker <clintonadeleke@gmail.com>
2022-03-16 14:22:51 +0000

    [FEAT]: Add path for elapsed time

--- src/pages/Home/components/Pomodoro/Timer.jsx
+++ src/pages/Home/components/Pomodoro/Timer.jsx
@@ -15,6 +15,16 @@ function Timer() {
       <svg className="timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <g className="timer__circle">
           <circle className="timer__path-elapsed" cx="50" cy="50" r="45" />
+          <path
+            className="timer__path-remaining"
+            strokeDasharray="283"
+            d="
+              M 50, 50
+              m -45, 0
+              a 45,45 0 1,0 90,0
+              a 45,45 0 1,0 -90,0
+            "
+          />
         </g>
       </svg>
       <span className="timer__label">{timeLeft}</span>
