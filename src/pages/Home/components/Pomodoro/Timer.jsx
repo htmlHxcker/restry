301585981136ecff40e810c83bf4f644340b1369
htmlHxcker <clintonadeleke@gmail.com>
2022-03-16 14:11:55 +0000

    [FEAT]:Update Timer.jsx to use countDown function

--- src/pages/Home/components/Pomodoro/Timer.jsx
+++ src/pages/Home/components/Pomodoro/Timer.jsx
@@ -1,27 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import countDown from './countDown';
 import './timer.scss';
-import formatTime from './useCountDown';
 
 function Timer() {
-  const [time, setTime] = useState(0);
-
-  const timeToCountFrom = 183;
-  let timeLeft = 0;
-  let timePassed = 0;
-  setInterval(() => {
-    timePassed += 1;
-    timeLeft = timeToCountFrom - timePassed;
-    setTime(formatTime(timeLeft));
-  }, 1000);
+  const [timeLeft, setTimeLeft] = useState('00:00');
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTimeLeft(countDown(183));
+    }, 1000);
+    return () => clearInterval(timerId);
+  }, []);
   return (
-
     <div className="timer">
       <svg className="timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <g className="timer__circle">
           <circle className="timer__path-elapsed" cx="50" cy="50" r="45" />
         </g>
       </svg>
-      <span className="timer__label">{time}</span>
+      <span className="timer__label">{timeLeft}</span>
     </div>
   );
 }
