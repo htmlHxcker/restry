1b5b84aec98252cf0ede9f4401d55504d254f664
htmlHxcker <clintonadeleke@gmail.com>
2022-03-17 15:19:10 +0000

    [FIX]:Change countdown.js to utils.js

--- src/pages/Home/components/Pomodoro/Timer.jsx
+++ src/pages/Home/components/Pomodoro/Timer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import countDown, { formatTime } from './countDown';
+import countDown, { formatTime } from './utils';
 import './timer.scss';
 
 const FULL_DASH_ARRAY = 283;
