8aaea8770ba9fd46ef6c7b7a3094fa1015aa42cb
htmlHxcker <clintonadeleke@gmail.com>
2022-03-22 15:43:22 +0000

    [FIX]:Allow dangling underscore

--- src/service-worker.js
+++ src/service-worker.js
@@ -24,7 +24,6 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
@@ -37,7 +36,7 @@ registerRoute(
       return false;
     } // If this looks like a URL for a resource, because it contains // a file extension, skip.
 
-    if (url.pathname.match(fileExtensionRegexp)) {
+    if (url.pathname.match('/[^/?]+\\.[^/]+$')) {
       return false;
     } // Return true to signal that we want to use the handler.
 
