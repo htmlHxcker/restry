7211f61ce2d2d705e1cbbca5fede20f2b54b4ced
htmlHxcker <clintonadeleke@gmail.com>
2022-03-22 13:04:58 +0000

    [FEAT}:Ability to add tasks

--- src/pages/Home/components/Tasks/index.jsx
+++ src/pages/Home/components/Tasks/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import ReactModal from 'react-modal';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 import TaskItem from './TaskItem';
 import girlWithLaptop from '../../assets/girl-with-laptop.png';
 import './index.scss';
@@ -7,6 +9,21 @@ import './index.scss';
 function Tasks() {
   const [tasks, setTasks] = useState(['Cook', 'Sleep', 'Laundry', 'Read', 'Netflix']);
   const [showModal, setShowModal] = useState(false);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const handleAddition = (data) => {
+    setTasks([...tasks, data.taskName]);
+  };
+  const formOptions = {
+    taskName: { required: 'Task Name is required' },
+    taskLength: {
+      required: 'You have to fill in how long you want to work',
+      minLength: {
+        value: 1,
+        message: 'Time has to be greater than 1',
+      },
+    },
+  };
   ReactModal.setAppElement('#root');
 
   return (
@@ -33,10 +50,20 @@ function Tasks() {
         </div>
 
         <div className="modal__form--container">
-          <form action="">
-            <input type="text" name="task name" />
+          <form onSubmit={handleSubmit(handleAddition)}>
+            <div>
+              <label htmlFor="taskName" className="form__label">What do you want to work on?</label>
+              <input type="text" name="taskName" {...register('taskName', formOptions.taskName)} className="form__input" />
+              <ErrorMessage errors={errors} name="taskName" as="p" className="error-message" />
+            </div>
+            <div>
+              <label htmlFor="taskLength" className="form__label">For how long?</label>
+              <input type="number" name="taskLength" {...register('taskLength', formOptions.taskLength)} className="form__input" />
+              <ErrorMessage errors={errors} name="taskLength" as="p" className="error-message" />
+            </div>
+
+            <button type="submit" className="primary--btn">Create Task</button>
           </form>
-          <button type="button" onClick={() => (setTasks([...tasks]))}>Test too</button>
         </div>
 
       </ReactModal>
