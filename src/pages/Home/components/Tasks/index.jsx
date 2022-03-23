510c57db79f2e6a01f15abb868ad3f022dedcfb5
htmlHxcker <clintonadeleke@gmail.com>
2022-03-23 15:31:13 +0000

    [FIX]:Remove React import

--- src/pages/Home/components/Tasks/index.jsx
+++ src/pages/Home/components/Tasks/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import ReactModal from 'react-modal';
 import { useForm } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
