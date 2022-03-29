b716e0c35bafcc40f88c06233130b0abec5004d6
htmlHxcker <clintonadeleke@gmail.com>
2022-03-29 18:03:26 +0100

    [FIX]:Create interface for props of TaskItem

--- src/pages/Home/components/Tasks/TaskItem.tsx
+++ src/pages/Home/components/Tasks/TaskItem.tsx
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
 import './TaskItem.scss';
 
-function TaskItem({ task }) {
+interface TaskItemProps {
+  task: string
+}
+
+function TaskItem({ task }: TaskItemProps) {
   return (<li className="task__item text--dark-blue">{task}</li>);
 }
 TaskItem.propTypes = {
