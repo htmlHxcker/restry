3f3f8b335cc68186c01f42e6e45520410bb0d5cc
htmlHxcker <clintonadeleke@gmail.com>
2022-03-30 10:24:10 +0100

    FEAT:Update TskItemProps interface to allow object

--- src/pages/Home/components/Tasks/TaskItem.tsx
+++ src/pages/Home/components/Tasks/TaskItem.tsx
@@ -1,13 +1,26 @@
 import PropTypes from 'prop-types';
+import { DotsHorizontalIcon } from '@heroicons/react/outline';
 import './TaskItem.scss';
 
 interface TaskItemProps {
-  task: string
+  task: {
+    taskName: string,
+    taskLength: number,
+    done: boolean
+  }
 }
 
 function TaskItem({ task }: TaskItemProps) {
-  return (<li className="task__item text--dark-blue">{task}</li>);
+  return (
+    <li className="task__item text--dark-blue flex justify-between">
+      {task.taskName}
+      <span>
+        <DotsHorizontalIcon className="task__item__menu" />
+      </span>
+    </li>
+  );
 }
+
 TaskItem.propTypes = {
   task: PropTypes.string.isRequired,
 };
