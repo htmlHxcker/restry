e9eb2445ca5246a2719634996067d42216d96a09
htmlHxcker <clintonadeleke@gmail.com>
2022-03-30 10:36:20 +0100

    [FIX]:Update icons for pomodoro controls

--- src/pages/Home/components/Pomodoro/index.tsx
+++ src/pages/Home/components/Pomodoro/index.tsx
@@ -1,3 +1,4 @@
+import { PauseIcon, PlayIcon } from '@heroicons/react/outline';
 import './index.scss';
 import Timer from './Timer';
 
@@ -6,9 +7,9 @@ function Pomodoro() {
     <div className="pomodoro">
       <Timer />
       <div className="flex">
-        <button type="submit" className="button--primary">▶</button>
-        <button type="submit" className="button--primary">⏸</button>
-        <button type="submit" className="button--primary">Start Pomodoro</button>
+        <button type="button" className="button--primary" aria-label="Button to start Pomodoro timer"><PlayIcon className="pomodoro__control-icons" /></button>
+        <button type="submit" className="button--primary" aria-label="Button to pause Pomodoro timer"><PauseIcon className="pomodoro__control-icons" /></button>
+        <button type="submit" className="button--primary" aria-label="Button to start Pomodoro timer">Start Pomodoro</button>
       </div>
     </div>
   );
