6b06da18bf4762c807e83853cbf5fb95b8737a41
htmlHxcker <clintonadeleke@gmail.com>
2022-04-14 20:28:13 +0100

    [FEAT]:Create lint-staged configuration file

--- lint-staged.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*.{ts,tsx}': [
-    'eslint --max-warnings=0',
-    'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
-    () => 'tsc-files --noEmit',
-  ],
-  '*.{ts,tsx,json,scss}': ['prettier --write'],
-};
