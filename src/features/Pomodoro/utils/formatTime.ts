edc92da9e2b4083cf3eecd14be8fac18c3c6c933
htmlHxcker <clintonadeleke@gmail.com>
2022-04-30 19:57:22 +0100

    [FEAT]:Separate timer utils into their own files

---
+++ src/features/Pomodoro/utils/formatTime.ts
@@ -0,0 +1,5 @@
+export default function formatTime(time: number): string {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
