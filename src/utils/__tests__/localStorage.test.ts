793023bf6b61513de7257f61cc30af4896babd21
htmlHxcker <clintonadeleke@gmail.com>
2022-05-09 22:07:30 +0100

    [FEAT]:Create test for localStorage

---
+++ src/utils/__tests__/localStorage.test.ts
@@ -0,0 +1,21 @@
+import { createTask } from '../../test/test-utils';
+import { setArrayItem } from '../localStorage';
+
+beforeEach(() => {
+  localStorage.clear();
+  jest.clearAllMocks();
+});
+
+test('should set item to local storage', () => {
+  localStorage.clear();
+
+  const task = createTask();
+  // The task has to be stringified because setArrayItem converts
+  // the value argument to a JSON string before passing it to local storage
+  const stringifiedTask = JSON.stringify(task);
+  setArrayItem('task', task);
+
+  expect(localStorage.setItem).toHaveBeenLastCalledWith('task', stringifiedTask);
+  expect(localStorage.__STORE__['task']).toBe(stringifiedTask);
+  expect(Object.keys(localStorage.__STORE__)).toHaveLength(1);
+});
