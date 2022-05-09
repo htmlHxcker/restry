c9270a08c290db8427ac4e779924d85ef834e43e
htmlHxcker <clintonadeleke@gmail.com>
2022-05-09 22:11:08 +0100

    [FEAT]:Wrap test in describe block

--- src/utils/__tests__/localStorage.test.ts
+++ src/utils/__tests__/localStorage.test.ts
@@ -6,16 +6,18 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-test('should set item to local storage', () => {
-  localStorage.clear();
+describe('LocalStorage tests', () => {
+  test('should set item to local storage', () => {
+    localStorage.clear();
 
-  const task = createTask();
-  // The task has to be stringified because setArrayItem converts
-  // the value argument to a JSON string before passing it to local storage
-  const stringifiedTask = JSON.stringify(task);
-  setArrayItem('task', task);
+    const task = createTask();
+    // The task has to be stringified because setArrayItem converts
+    // the value argument to a JSON string before passing it to local storage
+    const stringifiedTask = JSON.stringify(task);
+    setArrayItem('task', task);
 
-  expect(localStorage.setItem).toHaveBeenLastCalledWith('task', stringifiedTask);
-  expect(localStorage.__STORE__['task']).toBe(stringifiedTask);
-  expect(Object.keys(localStorage.__STORE__)).toHaveLength(1);
+    expect(localStorage.setItem).toHaveBeenLastCalledWith('task', stringifiedTask);
+    expect(localStorage.__STORE__['task']).toBe(stringifiedTask);
+    expect(Object.keys(localStorage.__STORE__)).toHaveLength(1);
+  });
 });
