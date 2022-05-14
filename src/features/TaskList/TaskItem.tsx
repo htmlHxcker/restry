9d53027282524c2cc9e271905de290aced6079ad
htmlHxcker <clintonadeleke@gmail.com>
2022-05-14 22:07:05 +0100

    [FIX]:Stop task item text from pushing menu icons

--- src/features/TaskList/TaskItem.tsx
+++ src/features/TaskList/TaskItem.tsx
@@ -9,7 +9,7 @@ interface TaskItemProps {
 export function TaskItem({ taskName }: TaskItemProps) {
   return (
     <li className="task__item text--dark-blue flex justify-between" data-testid="task-item">
-      {taskName}
+      <p className="task__item__text">{taskName}</p>
       <span>
         <TrashIcon className="task__item__menu" />
         <PencilIcon className="task__item__menu" />
