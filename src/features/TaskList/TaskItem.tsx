98666b72451f86c21c5f56d8fe65a6a0266880d9
htmlHxcker <clintonadeleke@gmail.com>
2022-05-15 21:18:28 +0100

    [FEAT]:Wrap items in buttons

--- src/features/TaskList/TaskItem.tsx
+++ src/features/TaskList/TaskItem.tsx
@@ -11,11 +11,21 @@ export function TaskItem({ taskName }: TaskItemProps) {
     <li className="task__item text--dark-blue flex justify-between" data-testid="task-item">
       <p className="task__item__text">{taskName}</p>
       <span>
-        <button>
-          <TrashIcon className="task__item__menu" />
+        <button
+          type="button"
+          aria-label="Edit Task"
+          title="Edit Task"
+          className="task__item__button"
+        >
+          <PencilIcon />
         </button>
-        <button type="button">
-          <PencilIcon className="task__item__menu" />
+        <button
+          type="button"
+          aria-label="Delete Task"
+          title="Delete Task"
+          className="task__item__button"
+        >
+          <TrashIcon />
         </button>
       </span>
     </li>
