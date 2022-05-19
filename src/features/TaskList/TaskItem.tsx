258c7b6ca75e7c3e38bfb2ff9aaaaf1d74923576
htmlHxcker <clintonadeleke@gmail.com>
2022-05-19 17:59:51 +0100

    [FEAT]: MVP for deleteing tasks

--- src/features/TaskList/TaskItem.tsx
+++ src/features/TaskList/TaskItem.tsx
@@ -1,17 +1,26 @@
 import { PencilIcon, TrashIcon } from '@heroicons/react/outline';
 import PropTypes from 'prop-types';
+
 import './TaskItem.scss';
+import { Task } from './TaskList';
 
 interface TaskItemProps {
-  taskName: string;
+  task: Task;
+  updateTasks: (tasks: []) => void;
 }
 
-export function TaskItem({ taskName }: TaskItemProps) {
+export function TaskItem({ task, updateTasks }: TaskItemProps) {
+  const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+
+  const deleteTask = () => {
+    const tasksAfterDeletion = tasks.filter((filteredTask: []) => filteredTask.id !== task.id);
+    updateTasks(tasksAfterDeletion);
+  };
   return (
     <li className="task__item text--dark-blue flex justify-between" data-testid="task-item">
       <label className="form-control task__item__text flex">
         <input type="checkbox" name="checkbox" onClick={() => {}} />
-        <span>{taskName}</span>
+        <span className={`${task.done ? 'checked' : ''}`}>{task.name}</span>
       </label>
 
       <span>
@@ -28,6 +37,9 @@ export function TaskItem({ taskName }: TaskItemProps) {
           aria-label="Delete Task"
           title="Delete Task"
           className="task__item__button"
+          onClick={() => {
+            deleteTask();
+          }}
         >
           <TrashIcon />
         </button>
@@ -37,5 +49,6 @@ export function TaskItem({ taskName }: TaskItemProps) {
 }
 
 TaskItem.propTypes = {
-  taskName: PropTypes.string.isRequired,
+  task: PropTypes.array.isRequired,
+  updateTasks: PropTypes.func.isRequired,
 };
