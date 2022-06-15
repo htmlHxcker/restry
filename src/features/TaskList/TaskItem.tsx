48d8cb194126917091301f322e55f668a46d0c47
htmlHxcker <clintonadeleke@gmail.com>
2022-06-15 21:06:51 +0100

    [FEAT]:Ability to mark tasks as done

--- src/features/TaskList/TaskItem.tsx
+++ src/features/TaskList/TaskItem.tsx
@@ -15,19 +15,38 @@ interface TaskItemProps {
 export function TaskItem({ task, updateTasks }: TaskItemProps) {
   const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
 
-  const deleteTask = () => {
+  function deleteTask() {
     const taskToDelete = task.id;
     const filteredTasks = tasks.filter((task: Task) => task.id !== taskToDelete);
     setArrayItem('tasks', filteredTasks);
     updateTasks(filteredTasks);
     toast(`Task succesfully deleted`);
-  };
+  }
+  function completeTask() {
+    const taskToComplete = task;
+    const completedTask = tasks.map((task: Task) =>
+      task.id !== taskToComplete.id ? task : { ...taskToComplete, done: !task.done }
+    );
+    setArrayItem('tasks', completedTask);
+    updateTasks(completedTask);
+  }
   return (
     <li className="task__item text--dark-blue flex justify-between" data-testid="task-item">
       <label className="form-control task__item__text flex">
-        <input type="checkbox" name="checkbox" onClick={() => {}} />
-        <span className={`${task.done ? 'done' : ''}`}>
-          <input type="text" value={task.name} className="bg--light-blue" onChange={() => {}} />
+        <input
+          type="checkbox"
+          name="checkbox"
+          onClick={() => {
+            completeTask();
+          }}
+        />
+        <span>
+          <input
+            type="text"
+            value={task.name}
+            className={`bg--light-blue ${task.done ? 'done' : ''}`}
+            onChange={() => {}}
+          />
         </span>
       </label>
 
@@ -57,6 +76,6 @@ export function TaskItem({ task, updateTasks }: TaskItemProps) {
 }
 
 TaskItem.propTypes = {
-  task: PropTypes.array.isRequired,
+  task: PropTypes.object.isRequired,
   updateTasks: PropTypes.func.isRequired,
 };
