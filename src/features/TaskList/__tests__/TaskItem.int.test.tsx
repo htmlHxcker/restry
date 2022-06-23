02a977861c1dc59be479baa7caac67320ae7a5d7
htmlHxcker <clintonadeleke@gmail.com>
2022-06-23 18:45:08 +0100

    [FIX]: Create test for marking tasks as done

--- src/features/TaskList/__tests__/TaskItem.int.test.tsx
+++ src/features/TaskList/__tests__/TaskItem.int.test.tsx
@@ -1,17 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { taskGenerator } from '@/test/data-generator';
 
 import { TaskItem } from '../TaskItem';
 
 describe('Tests for TestItem component', () => {
-  test('should render tasks', () => {
-    const demoTask = taskGenerator();
-    const mockCallback = jest.fn();
+  const demoTask = taskGenerator();
+  const mockCallback = jest.fn();
 
+  test('should render task', () => {
     render(<TaskItem task={demoTask} updateTasks={mockCallback} />);
     const task = screen.getByDisplayValue(demoTask.name);
-
     expect(task).toBeInTheDocument();
+  });
+
+  test('should be able to mark as done', async () => {
+    render(<TaskItem task={demoTask} updateTasks={mockCallback} />);
+    const user = userEvent.setup();
+    await user.click(screen.getByLabelText('Mark as done'));
+    expect(demoTask.done).toBe(!demoTask.done);
   });
 });
