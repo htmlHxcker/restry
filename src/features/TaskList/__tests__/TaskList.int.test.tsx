07de0371340d55256f193bf9a10f5b73b6212c43
htmlHxcker <clintonadeleke@gmail.com>
2022-06-23 18:47:43 +0100

    [FIX]: Remove queries using getByTestid

--- src/features/TaskList/__tests__/TaskList.int.test.tsx
+++ src/features/TaskList/__tests__/TaskList.int.test.tsx
@@ -1,34 +1,34 @@
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { createTask } from '@/test/test-utils';
+import { taskGenerator } from '@/test/data-generator';
 
-import { TasksList } from '../TaskList';
+import { TasksList } from '../TasksList';
 
 describe('Tests for Task list', () => {
   test('should show illustration if there are no tasks', () => {
-    const { container } = render(<TasksList />);
-    const illustration = screen.getByAltText('No Tasks yet');
-    expect(container).toContainElement(illustration);
+    render(<TasksList />);
+    const illustration = screen.getByAltText('No tasks yet');
+    expect(illustration).toBeInTheDocument();
   });
 
   test('should be able to create tasks', async () => {
     const user = userEvent.setup();
-    const task = createTask();
+    const task = taskGenerator();
     render(<TasksList />);
 
     const taskForm = screen.getByPlaceholderText('What do you want to work on?');
     await user.click(taskForm);
-    await user.type(taskForm, task.taskName);
-    expect(taskForm).toHaveValue(task.taskName);
+    await user.type(taskForm, task.name);
+    expect(taskForm).toHaveValue(task.name);
+    await user.click(screen.getByLabelText('Create Task'));
+    const createdTask = screen.getByDisplayValue(task.name);
 
-    await user.click(screen.getByRole('button', { name: /Create Task/i }));
-    const createdTask = screen.getByText(task.taskName);
     expect(createdTask).toBeInTheDocument();
   });
   test('should fetch tasks from localstorage', () => {
     const { container } = render(<TasksList />);
-    const TaskItem = screen.getByTestId('task-item');
+    const TaskItem = screen.getByLabelText('Form input to edit task containing task name');
     expect(container).toContainElement(TaskItem);
   });
 });
