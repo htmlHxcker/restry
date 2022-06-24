6b3f9da7cc15a3296d2466f535af4aabf11bc6fa
htmlHxcker <clintonadeleke@gmail.com>
2022-06-24 17:14:08 +0100

    [FEAT]:Move testing of tasks to cypress

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,3 +1,5 @@
+import { taskGenerator } from '@/test/data-generator';
+
 describe('Home page', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -6,5 +8,25 @@ describe('Home page', () => {
   it('loads the home page', () => {
     cy.contains('Home');
     cy.contains('Start Pomodoro');
+  });
+
+  it('should render empty tasks list', () => {
+    cy.get('.tasks-list--empty').should('exist');
+  });
+  it('should be able to create tasks', () => {
+    const task = taskGenerator();
+    cy.get('[placeholder = "What do you want to work on?"]').type(task.name);
+    cy.contains('Create Task').click();
+    cy.get('.task__item').should('be.visible');
+    cy.get('[title="Edit Task"]').should('have.value', task.name);
+  });
+
+  it('should be able to mark as done', () => {
+    const task = taskGenerator();
+    cy.get('[placeholder = "What do you want to work on?"]').type(task.name);
+    cy.contains('Create Task').click();
+    cy.get('[type="checkbox"]').click();
+    cy.get('[type="checkbox"]').should('be.checked');
+    cy.get('[title="Edit Task"]').should('have.class', 'done');
   });
 });
