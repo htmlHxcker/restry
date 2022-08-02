7628aadbc7e3d9849c90de07d61238fdb13d8f3f
htmlHxcker <clintonadeleke@gmail.com>
2022-08-02 13:28:08 +0100

    [FEAT]:Add reset param to allow for timer reset

--- src/features/Pomodoro/utils/countDown.ts
+++ src/features/Pomodoro/utils/countDown.ts
@@ -1,10 +1,14 @@
 let timePassed = 0;
 
-export default function countDown(timeLimit: number): number {
+export default function countDown(timeLimit: number, reset?: boolean): number {
   if (timePassed >= timeLimit) {
     return 0;
   }
-  timePassed += 1;
+  if (reset === true) {
+    timePassed = 0;
+  } else {
+    timePassed += 1;
+  }
   const timeLeft = timeLimit - timePassed;
 
   return timeLeft;
