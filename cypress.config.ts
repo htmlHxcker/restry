040c2f9576159ca191b5881672cec7cd8a140a5b
htmlHxcker <clintonadeleke@gmail.com>
2022-08-02 16:00:29 +0100

    [FEAT]:Move rule against test video to root config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'cypress';
 export default defineConfig({
-  e2e: { baseUrl: 'http://localhost:3000', viewportWidth: 1024, viewportHeight: 660 },
+  e2e: { baseUrl: 'http://localhost:3000', viewportWidth: 1024, viewportHeight: 660, video: false },
 });
