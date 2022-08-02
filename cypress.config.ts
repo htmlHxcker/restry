0c3449d03f6401fc07cda209373eb07e072f7ea4
htmlHxcker <clintonadeleke@gmail.com>
2022-08-02 17:44:57 +0100

    [FEAT]:Specify supportFile and specPattern

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,11 @@
 import { defineConfig } from 'cypress';
 export default defineConfig({
-  e2e: { baseUrl: 'http://localhost:3000', viewportWidth: 1024, viewportHeight: 660, video: false },
+  e2e: {
+    supportFile: false,
+    specPattern: 'cypress/e2e/*.spec.{ts,tsx}',
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1024,
+    viewportHeight: 660,
+    video: false,
+  },
 });
