891624657a37481cbebee122383f7d88bb4b75d0
htmlHxcker <clintonadeleke@gmail.com>
2022-08-03 12:41:13 +0100

    [FEAT]:Create checkbox for toggling pomodoro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+
 import { Pomodoro } from '@/features/Pomodoro';
 import { TasksList } from '@/features/TaskList';
+import './index.scss';
 
 function Home() {
+  const [hidePomodoro, setHidePomodoro] = useState(true);
   return (
-    <div className="flex items-center">
-      <TasksList />
-      <Pomodoro />
-    </div>
+    <>
+      <input
+        type="checkbox"
+        name="Mobile view toggle"
+        className="view-toggle"
+        onClick={() => {
+          setHidePomodoro(!hidePomodoro);
+        }}
+      />
+      <label htmlFor="Mobile view">Toggle View</label>
+
+      <div className={`flex items-center ${hidePomodoro ? 'hide-pomodoro' : 'show-pomodoro'}`}>
+        <TasksList />
+        <Pomodoro />
+      </div>
+    </>
   );
 }
 
