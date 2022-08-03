45a8b4e03bc360fac5795452d47909f045a9cad0
htmlHxcker <clintonadeleke@gmail.com>
2022-08-03 21:39:28 +0100

    [FIX]:Make toggle a button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 
 import { Pomodoro } from '@/features/Pomodoro';
@@ -8,16 +10,24 @@ function Home() {
   const [hidePomodoro, setHidePomodoro] = useState(true);
   return (
     <>
-      <input
-        type="checkbox"
-        name="Mobile view toggle"
-        className="view-toggle"
-        aria-label="Toggle View"
+      <button
+        className="view-toggle button--primary"
         onClick={() => {
           setHidePomodoro(!hidePomodoro);
         }}
-      />
-
+      >
+        {hidePomodoro ? (
+          <>
+            <FontAwesomeIcon icon={solid('clock')} />
+            <span> Show Timer</span>
+          </>
+        ) : (
+          <>
+            <FontAwesomeIcon icon={solid('list-alt')} />
+            <span> Show Tasks</span>
+          </>
+        )}
+      </button>
       <div className={`flex items-center ${hidePomodoro ? 'hide-pomodoro' : 'show-pomodoro'}`}>
         <TasksList />
         <Pomodoro />
